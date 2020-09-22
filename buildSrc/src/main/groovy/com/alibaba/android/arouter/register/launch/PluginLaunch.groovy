c88c19535c2b24080e3e861cf3b40beb8c71966d
zhangmingping <mingping.zhang@uurobot.cn>
2020-09-22 16:13:25 +0800

    javaProject

--- buildSrc/src/main/groovy/com/alibaba/android/arouter/register/launch/PluginLaunch.groovy
+++ buildSrc/src/main/groovy/com/alibaba/android/arouter/register/launch/PluginLaunch.groovy
@@ -1,5 +1,6 @@
 package com.alibaba.android.arouter.register.launch
 
+import com.alibaba.android.arouter.register.core.MyRegisterTransform
 import com.alibaba.android.arouter.register.utils.Logger
 import com.android.build.gradle.AppExtension
 import com.android.build.gradle.AppPlugin
@@ -16,17 +17,21 @@ public class PluginLaunch implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
+        Logger.make(project)
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
         def isApp = project.plugins.hasPlugin(AppPlugin)
-        Logger.i('Project enable arouter-register plugin$AppPlugin')
+        Logger.w('Project enable arouter-register plugin$AppPlugin')
         //only application module needs this plugin to generate register code
         if (isApp) {
+            System.out.println("AAAAAAAAAAAAAAA-isApp-AAAAAAAAAAAAAAAAAAA")
             Logger.make(project)
 
-            Logger.i('Project enable arouter-register plugin')
+            Logger.e('Project enable arouter-register plugin')
 
             def android = project.extensions.getByType(AppExtension)
+
             //register this plugin
-//            android.registerTransform(transformImpl)
+            android.registerTransform(new MyRegisterTransform(project))
         }
     }
 
